ae8d6ceb976c717006e23b1b6a2ce1188fa3fcab
BuildTools <unconfigured@null.spigotmc.org>
2023-01-21 12:34:24 -0700

    did the stuff

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import './App.css';
 import Nav from './components/Nav'
 import ContentWrapper from './components/ContentWrapper'
 import Footer from './components/Footer'
+import { lightMode, darkMode } from './ducks/toggleModeSlice';
+import { useDispatch, useSelector } from 'react-redux';
 
 function App() {
-  
+  const dispatch = useDispatch()
+  const mode = useSelector(state => state.mode)
+
   return (
-    <div style={{ backgroundColor: 'white', color: 'black' }} className="App">
+    <div style={{ backgroundColor: mode.color1, color: 'black' }} className="App">
       <Nav />
+      <button onClick={() => mode.darkMode ? dispatch(lightMode()) : dispatch(darkMode())}>{mode.darkMode ? "Light" : "Dark"} Mode</button>
       <ContentWrapper />
       <Footer />
     </div>
